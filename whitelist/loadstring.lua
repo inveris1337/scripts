fa125a5e33966a04c1c30ea92d72dd224ecc78f0
inveris1337 <j7130058@gmail.com>
2025-07-08 05:16:45 +0700

    Update loadstring.lua

--- whitelist/loadstring.lua
+++ whitelist/loadstring.lua
@@ -3,5 +3,5 @@
 getgenv().key = ""
 getgenv().intro_enabled = false
 
-getgenv().build = "niggajustletmesleep" --// do not change until i tell you to.
+getgenv().build = "crackme" --// do not change until i tell you to.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/inveris1337/scripts/refs/heads/main/whitelist/script.lua",true))()
