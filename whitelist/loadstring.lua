271add0c229128b6d1ac08e19a2b216f6791290a
inveris1337 <j7130058@gmail.com>
2025-07-07 22:09:06 +0700

    Create loadstring.lua

---
+++ whitelist/loadstring.lua
@@ -0,0 +1,7 @@
+--// retarded version of lua armor
+
+getgenv().key = ""
+getgenv().intro_enabled = false
+
+getgenv().build = "niggajustletmesleep" --// do not change until i tell you to.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/inveris1337/scripts/refs/heads/main/whitelist/script.lua",true))()
